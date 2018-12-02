4ec6c87a7b01cc5708e9c28e190aa95046e15c5a
TH <thow123@gmail.com>
2018-12-02 10:08:18 +0000

    Solution to Cooking Bug
    Hard-coded in a solution to the "Cooking Bug" as defined in the Evernote below.
    
    https://www.evernote.com/shard/s59/sh/3b86c257-0cf9-413c-b241-043ff28b0898/1f494714cb52b5843c0a683038307b4c

--- zibble2.py
+++ zibble2.py
@@ -18,7 +18,9 @@ parser.add_argument('--evernote', action='store_true')
 
 parser.add_argument('--folder', dest="input_folder")
 
-parser.add_argument('--nbook', dest="target_notebook")
+# Commenting this out because of the "Cooking Bug". Parent Notebook will now be hard-coded to equal the GUID of the Reading Notebook.
+
+# parser.add_argument('--nbook', dest="target_notebook")
 
 args = parser.parse_args()
 
@@ -168,13 +170,17 @@ def dict_writer(dict_list, filename):
 
 # This function is only used in the run_with_evernote script.
 
-def makeNote(authToken, noteStore, noteTitle, list_of_dicts, parentNotebook):
+def makeNote(authToken, noteStore, noteTitle, list_of_dicts):
 
     client = EvernoteClient(token=authToken, sandbox=False)
 
     noteStore = client.get_note_store()
 
     Errors = client.get_user_store()
+
+    # This is the GUID for the "Reading" notebook in my Evernote account. I am hardcoding this because of a bug with the --nbook option that I have not been able to diagnose.
+
+    Reading_guid = "ea612952-d3a5-4a39-862b-01190ba02e47"
 
     nBody = '<?xml version="1.0" encoding="UTF-8"?>'
     nBody += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
@@ -195,28 +201,22 @@ def makeNote(authToken, noteStore, noteTitle, list_of_dicts, parentNotebook):
     readingNote = ttypes.Note()
     readingNote.title = noteTitle
     readingNote.content = nBody
+    readingNote.notebookGuid = Reading_guid
 
     # Added this code because I couldn't get the stuff on lines 196-97 working. 
 
     # readingNote.notebookGuid = parentNotebook
 
-    # A new attempt to allow the user to match notebooks based on a name provided above. If this works, I will delete 187-191. 
+    # Commenting the code below out because I have been having a hard time getting the --nbook option to work (see "Cooking Bug")
 
-    account_notebooks = noteStore.listNotebooks()
+    # account_notebooks = noteStore.listNotebooks()
 
-    for notebook in account_notebooks:
-        try:
-            notebook.name == parentNotebook
-            readingNote.notebookGuid = notebook.guid
-        except:
-            print ("Notebook not found")
-
-    ## parentNotebook is optional; if omitted, default notebook is used
-
-    ## This bit of code below is not working, but was copied verbatim from the Evernote create note example.
-
-    ##if parentNotebook and hasattr(parentNotebook, 'guid'):
-        ##readingNote.notebookGuid = parentNotebook.guid
+    # for notebook in account_notebooks:
+    #     try:
+    #         notebook.name == parentNotebook
+    #         readingNote.notebookGuid = notebook.guid
+    #     except:
+    #         print ("Notebook not found")
 
     ## Attempt to create note in Evernote account
 
@@ -257,7 +257,9 @@ def run_with_evernote():
 
     directory = args.input_folder
 
-    parent_notebook = args.target_notebook
+    # Commenting this out because of the "Cooking Bug". Parent Notebook will now be hard-coded to equal the GUID of the Reading Notebook.
+
+    # parent_notebook = args.target_notebook
 
     for f in [f for f in os.listdir(directory) if f.endswith(".html")]:
         name = os.path.splitext(f)[0]
@@ -267,7 +269,7 @@ def run_with_evernote():
         extracted_heads = extract_headers(header_divs)
         extracted_bodies = extract_body(body_divs)
         heads_and_bodies = merge_heads_with_bodies(extracted_heads, extracted_bodies)
-        makeNote(my_token, my_store_URL, name, heads_and_bodies, parent_notebook)
+        makeNote(my_token, my_store_URL, name, heads_and_bodies)
 
 if (args.evernote):
     run_with_evernote()
